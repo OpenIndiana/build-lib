f6dd4bda87c3a572033092f916f63a2f8537c20e
Till Wegmueller <toasterson@gmail.com>
2023-04-12 20:00:56 +0200

    Show Build configuration in output log
    Signed-off-by: Till Wegmueller <toasterson@gmail.com>

--- vars/withPublisher.groovy
+++ vars/withPublisher.groovy
@@ -1,6 +1,8 @@
 def call(String publisher = 'openindiana.org', String bass_o_matic_mode = 'incremental', Closure body) {
 	withEnv(["PUBLISHER=${publisher}", 'GOCACHE=/tmp/.cache/go-build', 'USERLAND_ARCHIVES=/var/tmp/userland-archives/', 'CRYPTOGRAPHY_ALLOW_OPENSSL_102=true', "BASS_O_MATIC_MODE=${bass_o_matic_mode}", 'GNUMAKEFLAGS=--no-print-directory']) {
 		ws('/var/lib/abuild/ws-oi-userland') {
+			echo "Build mode: ${bass_o_matic_mode}"
+			echo "Building with publisher: ${publisher}"
     		body()
 		}
 	}
