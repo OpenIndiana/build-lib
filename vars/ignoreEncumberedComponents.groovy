11baf6436453fac8d8f543ecab09234ebc99ed5a
Till Wegmueller <toasterson@gmail.com>
2023-04-13 19:36:34 +0200

    Fix: ignore component helper
    Signed-off-by: Till Wegmueller <toasterson@gmail.com>

--- vars/ignoreEncumberedComponents.groovy
+++ vars/ignoreEncumberedComponents.groovy
@@ -1,5 +1,4 @@
-def call(String[] ignore_list) {
-
+def call(ignore_list) {
 	for(component in ignore_list){
 		echo "Ignoring ${component}"
 		writeFile file: 'components/encumbered/components.ignore', text: "\n/^${component}\$/d"
