0c855250cdf9be94951c76c86ab2e36ed35ae547
Till Wegmueller <toasterson@gmail.com>
2023-10-03 16:49:14 +0200

    Fix logic error
    Signed-off-by: Till Wegmueller <toasterson@gmail.com>

--- vars/pkgcopyEncumbered.groovy
+++ vars/pkgcopyEncumbered.groovy
@@ -1,6 +1,6 @@
 def call(String repo = 'hipster'){
 	ws('/var/lib/abuild/ws-oi-userland') {
-		sh "pkgrepo -s i386/encumbered-repo remove '*'"
 		sh "pkgrecv -s i386/encumbered-repo -d /zdata/oirepo/${repo} '*'"
+		sh "pkgrepo -s i386/encumbered-repo remove '*'"
 	}
 }
